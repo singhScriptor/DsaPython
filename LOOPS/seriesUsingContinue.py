49208eafdaa06e1c2321e4b7ea5e065c18d60a2e
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-11-26 20:57:47 +0530

    series using continue statement and recursion two methods

---
+++ LOOPS/seriesUsingContinue.py
@@ -0,0 +1,28 @@
+def print_Series(n):
+    i=1 
+    while(i<=n):
+        if(i%5==0):
+            i+=1
+            continue
+        print(i)
+        i+=1
+n=7
+print_Series(n)      
+
+# recursion
+
+def series(n):
+    def loop(i):
+        if(i>n): 
+            return
+        if (i%5 != 0):
+            print(i)
+        i+=1
+        loop(i)
+    loop(1)        
+def main():
+    m=7
+    series(m)
+main()        
+
+
