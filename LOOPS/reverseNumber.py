b1d853227c45ede01a7ad8f842a6812eefca7c32
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-11-26 21:56:29 +0530

    reverse the number

---
+++ LOOPS/reverseNumber.py
@@ -0,0 +1,11 @@
+def reverseNumber(n):
+    rev=0
+    while(n):
+        temp=n%10
+        rev=rev*10+temp
+        n=n//10
+    return rev
+def main():
+    n=321
+    print(reverseNumber(n))
+main()        
