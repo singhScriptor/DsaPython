f12e8220689b19266b521fd897a4718f404e3ce9
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-11-26 17:56:36 +0530

    printing even series loop using while loop and recursion method

--- LOOPS/EvenNubmber.py
+++ LOOPS/EvenNubmber.py
@@ -7,3 +7,18 @@ def evenNumber(n):
         i+=1
 n=10
 evenNumber(n)            
+
+
+# recursion
+
+def printEven(n):
+    def odd(i):
+        if(i>n):
+            return 
+        print(i)
+        odd(i+2)
+    odd(2)    
+def main():
+    m=14
+    printEven(m)
+main()    
