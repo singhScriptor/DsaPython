95009ade04a8462540f429b8ae2296a32f22ec57
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-11-26 15:16:22 +0530

    sum of two numbers also added recursion method

--- BASICS/sum.py
+++ BASICS/sum.py
@@ -1,12 +1,14 @@
 def sum(a,b):
     return a+b
-a,b=10,30
+a,b=7,-3
 print(sum(a,b))
 
 # recursion
 def recursiveSum(m,d):
     if(d==0):
         return m
-    return recursiveSum(m+1,d-1)
-m,d=34,67
+    elif d > 0:
+        return recursiveSum(m+1,d-1)
+    return recursiveSum(m-1,d+1)
+m,d=7,-4
 print(recursiveSum(m,d))
