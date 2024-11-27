f36cc8cbd734be7d542db463076cbf1105b1fb98
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-11-27 21:27:05 +0530

    odd number using while and for loop

---
+++ LOOPS/oddNumber.py
@@ -0,0 +1,18 @@
+def oddNumber(n):
+    i=0
+    while(i<=n):
+        if(i%2!=0):
+            print(i)
+        i+=1
+n=7
+oddNumber(n)
+
+# for loop
+def ODD(n):
+    for i in range(1,n+1):
+        if(i%2!=0):
+            print(i)
+n=7
+ODD(n)            
+
+
