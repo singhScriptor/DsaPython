8ad04757fc850091515893583243b956b3fd4774
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-11-28 17:59:46 +0530

    adding pattern using for loop and while loop

---
+++ LOOPS/printPattern.py
@@ -0,0 +1,22 @@
+def printPattern(n):
+    i=0
+    while(i<n):
+        j=i
+        str='*'
+        while(j<n-1):
+            str+="*"
+            j=j+1
+        print(str)
+        i+=1
+n=5
+printPattern(n)
+
+# forloop
+def printPattern(m):
+    for i in range(m):
+        str='*'
+        for j in range(m-i-1):
+            str+="*"
+        print(str)
+m=5
+printPattern(m)            
